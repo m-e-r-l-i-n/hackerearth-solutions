9d5a0428d419e757da47e2afef7b7ac6359a90d5
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-06-19 13:12:02 +0530

    Update Hasan and Trip.java

--- algorithms/dynamic programming/introduction to dynamic programming/Hasan and Trip.java
+++ algorithms/dynamic programming/introduction to dynamic programming/Hasan and Trip.java
@@ -1,32 +1,28 @@
 problem link-https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/practice-problems/algorithm/hasan-and-trip/?layout=old
 
 import java.io.*;
-
+ 
 class TestClass {
     public static void main(String args[] ) throws Exception {
         BufferedReader bu=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(bu.readLine()),i,a[][]=new int[n][3];
-        double max=0;
+        int n=Integer.parseInt(bu.readLine()),i,a[][]=new int[n][3],j;
+        double dp[]=new double[n];
         for(i=0;i<n;i++)
         {
             String s[]=bu.readLine().split(" ");
             a[i][0]=Integer.parseInt(s[0]);
             a[i][1]=Integer.parseInt(s[1]);
             a[i][2]=Integer.parseInt(s[2]);
-            max+=a[i][2];
-            if(i>0)
-            max-=Math.sqrt(Math.pow(a[i][0]-a[i-1][0],2)+Math.pow(a[i][1]-a[i-1][1],2));
+            dp[i]=Long.MIN_VALUE;
         }
-        int prev=0;
-        for(i=1;i<n-1;i++)
+        dp[0]=a[0][2];
+        for(i=1;i<n;i++)
         {
-            double val=max-a[i][2];
-            val+=Math.sqrt(Math.pow(a[i][0]-a[prev][0],2)+Math.pow(a[i][1]-a[prev][1],2));
-            val+=Math.sqrt(Math.pow(a[i+1][0]-a[i][0],2)+Math.pow(a[i+1][1]-a[i][1],2));
-            val-=Math.sqrt(Math.pow(a[i+1][0]-a[prev][0],2)+Math.pow(a[i+1][1]-a[prev][1],2));
-            if(val>max) max=val;
-            else prev=i;
+            for(j=0;j<i;j++)
+            dp[i]=Math.max(dp[i],dp[j]-Math.sqrt(1l*(a[j][0]-a[i][0])*(a[j][0]-a[i][0])+
+            1l*(a[j][1]-a[i][1])*(a[j][1]-a[i][1])));
+            dp[i]+=a[i][2];
         }
-        System.out.format("%.6f",max);
+        System.out.format("%.6f",dp[n-1]);
     }
 }
